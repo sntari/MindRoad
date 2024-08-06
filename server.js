2edc4edc780ce29de7235f135b5b2a94a7e157c1
sntari <kdh39554@gmail.com>
2024-08-06 11:43:13 +0900

    환경구축

---
+++ server.js
@@ -0,0 +1,48 @@
+// express 모듈 불러오기
+// express : node.js에서 웹 어플리케이션 구축할때 사용하는 프레임워크
+// 서버설정, 라우팅, 미들웨어 처리 기능 제공
+const express = require('express');
+
+// ejs의 레이아웃기능을 제공하여 유지보수에 용이
+const expressLayouts = require('express-ejs-layouts')
+
+// 세션 관리를 위한 미들웨어
+// 사용자별 세션 데이터를 서버에 저장 
+// 세션 ID를 쿠키로 클라이언트에 전달하여 상태를 유지
+var session = require('express-session');
+const path = require('path');
+var cookieParser = require('cookie-parser');
+
+//  Express에서 HTTP 요청의 본문(body)을 파싱하는 미들웨어
+// 주로 POST 요청의 본문 데이터를 처리
+var bodyParser = require('body-parser');
+
+// express 객체 생성
+const app = express();
+
+// ejs를 템플릿 엔진으로 사용하도록 설정
+app.use(expressLayouts);
+
+// ejs를 템플릿 엔진으로 사용하도록 설정
+app.set('view engine', 'ejs');
+app.set('layout', 'template');
+// 뷰 템플릿 파일이 위치한 디렉토리를 public으로 설정
+app.set('views', path.join(__dirname, 'public', 'views'));
+
+// 정적 파일을 제공하기 위한 미들웨어 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
+// POST 요청 본문을 파싱하기 위한 미들웨어 설정
+app.use(bodyParser.urlencoded({ extended: true }));
+// HTTP 요청의 body에서 JSON 형식으로 전송된 데이터를 파싱(parsing)하여 JavaScript 객체로 만들어주는 역할 
+app.use(bodyParser.json());
+
+// 메인페이지
+app.get('/', (req, res) => {
+      res.render('main');
+  });
+
+app.listen(3300, () => {
+    console.log(`서버 가동`);
+  });
+  
