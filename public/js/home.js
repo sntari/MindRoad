3a1c402344d9119dc2bf748a3acac7675c4ad868
sntari <kdh39554@gmail.com>
2024-09-03 16:41:32 +0900

    코드수정

--- public/js/home.js
+++ public/js/home.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	function createMessage() {
 		const messageDiv = document.createElement('div');
 		messageDiv.className = 'message bot-message';
-		messageDiv.innerText = 'loading...';
+		// messageDiv.innerText = 'loading...';
 		chatContent.appendChild(messageDiv);
 		return messageDiv;
 	}
@@ -72,7 +72,10 @@ document.addEventListener('DOMContentLoaded', function () {
 			const botMessage = createMessage(); //add
 			try {
 				const botResponse = await getFlaskResponse(message);
-				addMessage(botResponse, false);
+				addMessage(botResponse.answer, false);
+				if(botResponse.isProblem){
+					await saveChatbotResponseToNodeServer(botResponse);
+				}
 			} catch (error) {
 				// addMessage("죄송합니다. 오류가 발생했습니다: " + error.message, false); //remove
 				typeMessage(botMessage, "죄송합니다. 오류가 발생했습니다: " + error.message);
